bfbad4b3be88aaf040a8e1967dfab95b68789c14
Adeeb-7067 <adeebkhan3463@gmail.com>
2026-05-30 12:22:00 +0530

    fix case-sensitivity import for loanAssignment.png

--- src/lib/constant.js
+++ src/lib/constant.js
@@ -2,7 +2,7 @@ import DashboardIcon from "../Assets/dashboardIcon.png";
 import ConfigIcon from "../Assets/configuration.png";
 import EmployeeIcon from "../Assets/employeeIcon.png";
 import AttendanceIcon from "../Assets/attendenceIcon.png";
-import LoanAssignment from "../Assets/LoanAssignment.png";
+import LoanAssignment from "../Assets/loanAssignment.png";
 import AttendanceRegularization from "../Assets/attendanceRegularization.png";
 import PayrollDashboard from "../Assets/payrollDashboard.png";
 import EmployeeDashboard from "../Assets/EmployeeDashboard.png";
